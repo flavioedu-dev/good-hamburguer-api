afa465043662c82aa0b5755ee4720d23b1a1b524
Flávio Eduardo <xflax00@gmail.com>
2026-04-21 13:05:35 -0300

    feat: adiciona migration que torna coluna UpdatedAt nulável

--- GoodHamburger.Domain/Entities/Base/BaseEntity.cs
+++ GoodHamburger.Domain/Entities/Base/BaseEntity.cs
@@ -5,5 +5,5 @@ public class BaseEntity
     public int Id { get; set; }
     public bool Active { get; set; } = true;
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-    public DateTime UpdatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
 }
