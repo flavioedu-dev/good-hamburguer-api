2f106cada5dcde8d10cf813303ce5aa52ed91877
Flávio Eduardo <xflax00@gmail.com>
2026-04-21 00:06:22 -0300

    feat: implementa contexto sql

---
+++ GoodHamburger.Infrastructure/Contexts/SqlDbContext.cs
@@ -0,0 +1,20 @@
+﻿using GoodHamburger.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GoodHamburger.Infrastructure.Contexts;
+
+public class SqlDbContext : DbContext
+{
+    public DbSet<Product> Products { get; }
+    public DbSet<OrderItem> OrderItems { get; }
+    public DbSet<Order> Orders { get; }
+
+    public SqlDbContext(DbContextOptions<SqlDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
